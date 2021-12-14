664d3a58c90b500e100fd13e75c713789080454e
goswami-hub <budhagoswami172@gmail.com>
2021-12-14 22:57:27 +0100

    adding three ML models

--- ml_model.py
+++ ml_model.py
@@ -1,2 +1,11 @@
 
 print("Add all ML related codes here")
+
+def model1():
+	print('add code for model 1')
+
+def model2():
+	print('add code for model 2')
+
+def model3():
+	print('add code for model 3')
